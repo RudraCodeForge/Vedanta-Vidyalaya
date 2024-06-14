9041469ea0b147518f9427df581eb92b7e718e9d
Prince Daksh <149432297+RudraCodeForge@users.noreply.github.com>
2024-06-14 18:07:38 +0000

    Add Navbar design

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,20 @@
+import './Navbar.css';
+
 import Container from "../template/Container.jsx"
 const Navbar=()=>{
   return (
     <Container className="navbar-container">
-      <div>hello</div>
+      <div className="logo">
+      </div>
+      
+      <div className="Hammburger">
+      </div>
+
+      <div className="Menu">
+        <a href="#">HOME</a>
+        <a href="#">BOOKS</a>
+        <a href="#">ABOUT-US</a>
+      </div>
     </Container>
   )
 }
