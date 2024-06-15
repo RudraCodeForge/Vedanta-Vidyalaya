572e1d2d2f2d2ff73c6035854ec079eb019fc4c9
Prince Daksh <149432297+RudraCodeForge@users.noreply.github.com>
2024-06-15 08:05:08 +0000

    Update code

--- src/Components/NavComponents/Hamburger.jsx
+++ src/Components/NavComponents/Hamburger.jsx
@@ -1,20 +1,31 @@
 import "./Hamburger.css";
 
+import {useState} from 'react';
+
+
+const HamburgerHandler = () => {
+  console.log(`message`);
+  
+};
+
+
 const Hamburger = () => {
   return (
     <div>
-      <div className="Hamburger">
+      <div id="Ham" className="Hamburger" onClick={HamburgerHandler}>
         <span className="bar"></span>
         <span className="bar"></span>
         <span className="bar"></span>
       </div>
 
-      <div className="Menu">
+      <div id="Data" className="Menu">
+        <input className="Search" type="text" placeholder="Search" />
         <a href="#">HOME</a>
         <a href="#">BOOKS</a>
         <a href="#">ABOUT-US</a>
       </div>
     </div>
   );
+  
 };
 export default Hamburger;
